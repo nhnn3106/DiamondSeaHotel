800027f9e414f00e23c2f86b5a8e2c79bcac89f7
nhnn3106 <120386489+nhnn3106@users.noreply.github.com>
2025-04-17 21:13:00 +0700

    bỏ justify-content-center

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -1,24 +1,28 @@
 import { Container, Alert } from "react-bootstrap";
 import RoomCard from "./RoomCard";
 import Lazyload from "react-lazyload";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const RoomList = ({ rooms }) => {
-  const newRoom = rooms.slice(0,12);
-  
+  const newRoom = rooms.slice(0, 12);
+
   if (rooms.length === 0) {
     return (
       <Container className="mt-5 pt-5 text-center">
         <Alert variant="info" className="p-5 shadow-sm">
           <h4 className="mb-3">Không tìm thấy phòng</h4>
-          <p>Không có phòng nào phù hợp với bộ lọc hiện tại.<br />Vui lòng thử lại với các tiêu chí khác.</p>
+          <p>
+            Không có phòng nào phù hợp với bộ lọc hiện tại.
+            <br />
+            Vui lòng thử lại với các tiêu chí khác.
+          </p>
         </Alert>
       </Container>
     );
   }
-  
+
   return (
-    <Container className="d-flex flex-wrap justify-content-center">
+    <Container className="d-flex flex-wrap">
       {newRoom.map((room) => (
         <Lazyload key={room.roomID} height={300} offset={100}>
           <RoomCard
@@ -38,7 +42,7 @@ const RoomList = ({ rooms }) => {
 
 // Add PropTypes validation
 RoomList.propTypes = {
-  rooms: PropTypes.array.isRequired
+  rooms: PropTypes.array.isRequired,
 };
 
 export default RoomList;
